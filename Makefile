5708dddb829389f800578fe2a2e1cbb695b849bc
Daisuke Sugiyama <dsugiyama@outlook.jp>
2015-01-09 06:20:09 +0900

    add build script

--- Makefile
+++
@@ -1,5 +0,0 @@
-all: lap.c lap-thread.c
-	xmpcc -O3 lap.c -o lap -lm
-	xmpcc -O3 -enable-threads lap-thread.c -o lap-thread -lm
-clean:
-	rm -f *.o *~ lap
---
+++ build.sh
@@ -0,0 +1,19 @@
+for src in *mpi*.c; do
+	body=${src%\.c}
+	exe=${body#__omni_tmp_}
+	mpiicc -mmic -O3 $src -o $exe.mic -lm -lxmp \
+	-I$HOME/omni-compiler/include -L$HOME/omni-compiler/lib \
+	-std=gnu99 -openmp -fno-alias
+done
+
+for src in *threads*.c; do
+	body=${src%\.c}
+	exe=${body#__omni_tmp_}
+	mpiicc -mmic -O3 $src -o $exe.mic -lm -lxmp -lpthread \
+	-I$HOME/omnixmp-threads/include -L$HOME/omnixmp-threads/lib \
+	-std=gnu99 -openmp -fno-alias
+done
+
+for src in *omp*.c; do
+	icc -mmic -O3 $src -o ${src%\.c}.mic -lm -std=gnu99 -openmp
+done
