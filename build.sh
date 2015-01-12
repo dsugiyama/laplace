3777617d4472e5ce1324e2982048cf3d6d924be6
Daisuke Sugiyama <dsugiyama@outlook.jp>
2015-01-13 07:46:17 +0900

    add truly non-vectorized versions

--- build.sh
+++ build.sh
@@ -1,23 +1,29 @@
 for src in *mpi*.c; do
 	body=${src%\.c}
 	exe=${body#__omni_tmp_}
-	mpiicc -mmic -O3 $src -o $exe.mic -lm -lxmp \
+	mpiicc -mmic -O3 $src -o ${exe}_noalias.mic -lm -lxmp \
 	-I$HOME/omni-compiler/include -L$HOME/omni-compiler/lib \
 	-std=gnu99 -openmp -fno-alias
-	mpiicc -mmic -O3 $src -o ${exe}_novec.mic -lm -lxmp \
+	mpiicc -mmic -O3 $src -o ${exe}.mic -lm -lxmp \
 	-I$HOME/omni-compiler/include -L$HOME/omni-compiler/lib \
 	-std=gnu99 -openmp
+	mpiicc -mmic -O3 $src -o ${exe}_novec.mic -lm -lxmp \
+	-I$HOME/omni-compiler/include -L$HOME/omni-compiler/lib \
+	-std=gnu99 -openmp -no-vec
 done
 
 for src in *threads*.c; do
 	body=${src%\.c}
 	exe=${body#__omni_tmp_}
-	mpiicc -mmic -O3 $src -o $exe.mic -lm -lxmp -lpthread \
+	mpiicc -mmic -O3 $src -o ${exe}_noalias.mic -lm -lxmp -lpthread \
 	-I$HOME/omnixmp-threads/include -L$HOME/omnixmp-threads/lib \
 	-std=gnu99 -openmp -fno-alias
-	mpiicc -mmic -O3 $src -o ${exe}_novec.mic -lm -lxmp -lpthread \
+	mpiicc -mmic -O3 $src -o ${exe}.mic -lm -lxmp -lpthread \
 	-I$HOME/omnixmp-threads/include -L$HOME/omnixmp-threads/lib \
 	-std=gnu99 -openmp
+	mpiicc -mmic -O3 $src -o ${exe}_novec.mic -lm -lxmp -lpthread \
+	-I$HOME/omnixmp-threads/include -L$HOME/omnixmp-threads/lib \
+	-std=gnu99 -openmp -no-vec
 done
 
 for src in *omp*.c; do
