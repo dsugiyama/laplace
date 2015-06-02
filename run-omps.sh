c93b1556a0110b0fa12f672dcd8419a027c10bae
Daisuke Sugiyama <dsugiyama@outlook.jp>
2015-06-03 00:20:42 +0900

    add execution scripts for Omni OpenMP and Argobots

---
+++ run-omps.sh
@@ -0,0 +1,19 @@
+sizes1d="1 2 4 8 16 32 60 120 240"
+
+niter=$1
+mic=$2
+
+. ./rundef.sh
+
+exec_abt() {
+    echo $1
+    for s in $sizes1d; do
+        python aggregate.py $niter $mic -- \
+        OMPC_NUM_FORK=$s ./$1
+    done
+    echo
+}
+
+exec_omp lap_intel
+exec_omp lap_omni
+exec_abt lap_abt
