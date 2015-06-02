44ad39f055a0330cbbf5cf1aa394956ca33d55ee
Daisuke Sugiyama <dsugiyama@outlook.jp>
2015-06-02 23:56:04 +0900

    separate shell function definitions

--- run.sh
+++ run.sh
@@ -4,54 +4,7 @@ sizes2d="1x1 1x2 2x2 2x4 4x4 4x8 6x10 10x12 15x16"
 niter=$1
 mic=$2
 
-exec_mpi_1d() {
-    echo $1
-    for s in $sizes1d; do
-        python aggregate.py $niter $mic -- \
-        XMP_NODE_SIZE0=$s mpirun -n $s ./$1
-    done
-    echo
-}
-
-exec_mpi_2d() {
-    echo $1
-    for s in $sizes2d; do
-        s0=${s%x*}
-        s1=${s#*x}
-        python aggregate.py $niter $mic -- \
-        XMP_NODE_SIZE0=$s0 XMP_NODE_SIZE1=$s1 mpirun -n $(($s0 * $s1)) ./$1
-    done
-    echo
-}
-
-exec_threads_1d() {
-    echo $1
-    for s in $sizes1d; do
-        python aggregate.py $niter $mic -- \
-        XMP_NODE_SIZE0=$s ./$1
-    done
-    echo
-}
-
-exec_threads_2d() {
-    echo $1
-    for s in $sizes2d; do
-        s0=${s%x*}
-        s1=${s#*x}
-        python aggregate.py $niter $mic -- \
-        XMP_NODE_SIZE0=$s0 XMP_NODE_SIZE1=$s1 ./$1
-    done
-    echo
-}
-
-exec_omp() {
-    echo $1
-    for s in $sizes1d; do
-        python aggregate.py $niter $mic -- \
-        OMP_NUM_THREADS=$s ./$1
-    done
-    echo
-}
+. ./rundef.sh
 
 exec_mpi_1d lap_mpi_1d-1K
 exec_mpi_2d lap_mpi_2d-1K
---
+++ rundef.sh
@@ -0,0 +1,48 @@
+exec_mpi_1d() {
+    echo $1
+    for s in $sizes1d; do
+        python aggregate.py $niter $mic -- \
+        XMP_NODE_SIZE0=$s mpirun -n $s ./$1
+    done
+    echo
+}
+
+exec_mpi_2d() {
+    echo $1
+    for s in $sizes2d; do
+        s0=${s%x*}
+        s1=${s#*x}
+        python aggregate.py $niter $mic -- \
+        XMP_NODE_SIZE0=$s0 XMP_NODE_SIZE1=$s1 mpirun -n $(($s0 * $s1)) ./$1
+    done
+    echo
+}
+
+exec_threads_1d() {
+    echo $1
+    for s in $sizes1d; do
+        python aggregate.py $niter $mic -- \
+        XMP_NODE_SIZE0=$s ./$1
+    done
+    echo
+}
+
+exec_threads_2d() {
+    echo $1
+    for s in $sizes2d; do
+        s0=${s%x*}
+        s1=${s#*x}
+        python aggregate.py $niter $mic -- \
+        XMP_NODE_SIZE0=$s0 XMP_NODE_SIZE1=$s1 ./$1
+    done
+    echo
+}
+
+exec_omp() {
+    echo $1
+    for s in $sizes1d; do
+        python aggregate.py $niter $mic -- \
+        OMP_NUM_THREADS=$s ./$1
+    done
+    echo
+}
